fb643811379e27541e1c233cfa9f769ecfec8b81
crivellito <augusto.crivelli03@gmail.com>
2025-05-19 19:40:08 -0300

    Agregando práctica de compilacion con varios fuent

---
+++ PRACTICO/Contruccion/Varios_fuentes/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "operaciones.h"
+
+int main (void) {
+
+	int a = 5, b = 3;
+	printf("Suma: %d\n", suma(a, b));
+	printf("Resta: %d\n", resta(a, b));
+	
+	return 0;
+}
+
---
+++ PRACTICO/Contruccion/Varios_fuentes/operaciones.h
@@ -0,0 +1,7 @@
+#ifndef OPERACIONES_H
+#define OPERACIONES_H
+
+int suma (int, int);
+int resta (int, int);
+
+#endif
